7af55865ed586e1a5b1f3457eb545c050e4028fb
gars-cmd <avidouille@gmail.com>
2021-11-28 18:01:54 +0200

    add a function to create graph

--- src/DWG.java
+++ src/DWG.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
+
 import api.DirectedWeightedGraph;
 import api.EdgeData;
 import api.NodeData;
@@ -9,14 +10,27 @@ public class DWG implements DirectedWeightedGraph {
 
 
     private ArrayList<Nodes> graph = new ArrayList<>();
+    private int nbrNodes=0;
+    private int nbrEdges=0;
 
 public DWG(ArrayList<Nodes> graph){
     
     for (int i = 0; i < graph.size(); i++) {
         this.graph.add(new Nodes(graph.get(i)));
+        nbrEdges+=this.graph.get(i).getEdgeList().size();
+        nbrNodes++;
     }
 
 }
+
+public DWG (ArrayList<Nodes> nodes, ArrayList<Edges> edges) { //another way to create a graph with two list nodes, edges to handle easily the read from json
+    for (int i = 0; i < edgeSize(); i++) {
+        int src = edges.get(i).getSrc();
+        double weight = edges.get(i).getWeight();
+        int dest = edges.get(i).getDest();
+        this.connect(src, dest, weight);
+    }
+} 
 
 
 
@@ -41,6 +55,7 @@ public DWG(ArrayList<Nodes> graph){
         Point_3D new_point = new Point_3D(n.getLocation().x(),n.getLocation().y(),n.getLocation().z());
         Nodes new_Node=new Nodes(new_point,this.graph.size());
         this.graph.add(new_Node);
+        nbrNodes++;
     
     }
 
@@ -48,6 +63,7 @@ public DWG(ArrayList<Nodes> graph){
     public void connect(int src, int dest, double w) {
         Edges new_edge = new Edges(src, w, dest);
         this.graph.get(src).getEdgeList().add(new_edge);
+        nbrEdges++;
         
     }
 
@@ -73,25 +89,31 @@ public DWG(ArrayList<Nodes> graph){
     @Override
     public NodeData removeNode(int key) {
         this.graph.remove(key);
+        nbrNodes--;
         return null;
     }
 
     @Override
     public EdgeData removeEdge(int src, int dest) {
-        // TODO Auto-generated method stub
+        for (int i = 0; i < this.graph.get(src).getEdgeList().size(); i++) {
+            if(this.graph.get(src).getEdgeList().get(i).getDest()==dest){
+                this.graph.get(src).getEdgeList().remove(i);
+                nbrEdges--;
+            } 
+         }
         return null;
     }
 
     @Override
     public int nodeSize() {
-        // TODO Auto-generated method stub
-        return 0;
+        return nbrNodes;
+        
     }
 
     @Override
     public int edgeSize() {
-        // TODO Auto-generated method stub
-        return 0;
+        return nbrEdges;
+        
     }
 
     @Override
