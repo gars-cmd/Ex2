941f5468b16497e982e2ae12f4953313b8e8cca8
1galaqua <gaqua61@gmail.com>
2021-12-09 22:14:03 +0200

    add test

---
+++ Test/DWGAlgoTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DWGAlgoTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void getGraph() {
+    }
+
+    @Test
+    void isConnected() {
+    }
+
+    @Test
+    void shortestPathDist() {
+    }
+
+    @Test
+    void shortestPath() {
+    }
+
+    @Test
+    void center() {
+    }
+
+    @Test
+    void tsp() {
+    }
+}
---
+++ Test/DWGTest.java
@@ -0,0 +1,52 @@
+import api.DirectedWeightedGraph;
+import api.NodeData;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DWGTest {
+
+
+
+
+
+    @Test
+    void getNode() {
+
+
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void edgeIter() {
+    }
+
+    @Test
+    void removeNode() {
+    }
+
+    @Test
+    void removeEdge() {
+    }
+
+    @Test
+    void nodeSize() {
+    }
+
+    @Test
+    void edgeSize() {
+    }
+}
